199947206a65747053d822f0e0f8b165e6d2e12c
Harsh Tripathi <139077253+helloharrsh@users.noreply.github.com>
2024-06-19 12:06:46 +0530

    Update LongestCommonSubstring.java

--- dynamicProgramming/StringProblems/LongestCommonSubstring.java
+++ dynamicProgramming/StringProblems/LongestCommonSubstring.java
@@ -38,16 +38,19 @@ public class LongestCommonSubstring {
 	
 	static int helper(String s1, String s2, int n, int m, int count) { 
 		
-		if(n == 0 || m ==0) {
-			return count; 
-		}
-		if(s1.charAt(n-1) == s2.charAt(m-1)) {
-			count = helper(s1,s2,n-1,m-1,count+1);
-		}
-		
-		count = Math.max(helper(s1,s2,n-1,m,0), helper(s1,s2,n,m-1,0));
-		
-		return count;
+	if (n == 0 || m == 0) {
+            return count;
+        }
+
+        int count1 = count;
+        if (s1.charAt(n - 1) == s2.charAt(m - 1)) {
+            count1 = helper(s1, s2, n - 1, m - 1, count + 1);
+        }
+
+        int count2 = helper(s1, s2, n - 1, m, 0);
+        int count3 = helper(s1, s2, n, m - 1, 0);
+
+        return Math.max(count1, Math.max(count2, count3));
 	}
 	
 	static void solveUsingMemo() {
